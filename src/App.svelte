cadadcffe17405c190a6c88c83aa454abe5e6b5c
tnharvey-tpwd <timothy.harvey@tpwd.texas.gov>
2026-05-18 16:07:30 -0500

    partial initial upload - src

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Navigation from "./components/Navigation.svelte";
+  import Relationships from "./pages/Relationships.svelte";
+  import ContactLog from "./pages/ContactLog.svelte";
+
+  let currentPage = "relationships";
+
+  function navigate(page) {
+    currentPage = page;
+  }
+</script>
+
+<div class="min-h-screen flex flex-col">
+  <Navigation {currentPage} on:navigate={(e) => navigate(e.detail)} />
+
+  <main class="p-6">
+    {#if currentPage === "relationships"}
+      <Relationships />
+    {:else if currentPage === "contactlog"}
+      <ContactLog />
+    {/if}
+  </main>
+</div>
