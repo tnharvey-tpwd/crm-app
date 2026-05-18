9de2b67f52dbcba9fa2e255a1b31a05d85f9f63a
tnharvey-tpwd <timothy.harvey@tpwd.texas.gov>
2026-05-18 16:08:23 -0500

    Update vite.config.js
    add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/crm-app/',  // <--- REQUIRED FOR GITHUB PAGES
   server: {
     open: true
   }
